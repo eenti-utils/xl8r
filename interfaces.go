9160efc3ba7850301dcef229faca0981f9cfb9b9
EENTI <85809852+eenti-utils@users.noreply.github.com>
2024-01-01 11:26:33 -0500

    Update interfaces.go
    update package description text

--- interfaces.go
+++ interfaces.go
@@ -1,4 +1,4 @@
-// Package xl8r facilitates development of "spoke and hub" paradigm translators
+// Package xl8r facilitates development of "spoke and hub" translators
 package xl8r
 
 type Interpreter[P, H any] interface {
